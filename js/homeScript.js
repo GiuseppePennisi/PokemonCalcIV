c6b33ff593903636fd8f4c16c91e20d83e1cb06d
Giuseppe Pennisi <giuseppe.pennisi11@gmail.com>
2018-08-23 22:08:37 +0200

    Display pokemon's images.

--- js/homeScript.js
+++ js/homeScript.js
@@ -1,11 +1,10 @@
 
-$.getJSON("https://pokeapi.co/api/v2/pokemon/?limit=12", function (pokemonList) {
+$.getJSON("https://pokeapi.co/api/v2/pokemon/?limit=949", function (pokemonList) {
 
 
-    console.log(pokemonList.results.length)
 
     for (i = 0; i < pokemonList.results.length; i=i+6) {
-        console.log("ciao");
+
         var rowElement = document.createElement("div");
         rowElement.setAttribute("class","row");
         rowElement.setAttribute("id", "row" + i/6);
@@ -17,18 +16,16 @@ $.getJSON("https://pokeapi.co/api/v2/pokemon/?limit=12", function (pokemonList) 
             col.setAttribute("class","col-sm");
             col.setAttribute("id",pokemonList.results[j].name);
             var namePokemon = pokemonList.results[j].name;
-            console.log(i+" "+j);
             col.innerHTML = pokemonList.results[j].name;
             $("#row"+i/6).append(col);
 
                 var img = document.createElement("img");
                 var url = pokemonList.results[j].url;
                 var idPokemon = url.match(/\d/g);
+                console.log("preShift "+idPokemon);
                 idPokemon.shift();
-                idPokemon.join("");
-                console.log(idPokemon);
+                idPokemon = idPokemon.join("");
                 img.setAttribute("src","../Sprites/"+idPokemon+".png");
-                console.log(namePokemon)
                 $("#"+namePokemon).append(img);
 
 
